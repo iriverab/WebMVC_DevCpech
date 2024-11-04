cdbea06dd50d35c0b7d40bbb52759db1d95623a1
angeles <shhob@DESKTOP-MDLFUJH>
2024-11-04 10:56:52 -0300

    ver

---
+++ WebMVC_DevCpech/Controllers/CostoController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using WebMVC_DevCpech.ContextDB;
+using WebMVC_DevCpech.Repository;
+
+
+namespace WebMVC_DevCpech.Controllers
+{
+    public class CostoController : Controller
+    {
+        private CentroDeCostoRepository _centroDeCostoRepo;
+
+        public CostoController()
+        {
+            _centroDeCostoRepo = new CentroDeCostoRepository();
+        }
+
+        public ActionResult Index()
+        {
+            var data = _centroDeCostoRepo.getCentroDeCostos();
+            //var dataCC = _centroDeCostoRepo.getCentroCostosAll();
+            return View(data);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        public ActionResult EditarCosto(int Codigo)
+        {
+            var objCentroDeCostos = _centroDeCostoRepo.getCentroDeCostosById(Codigo);
+            if (objCentroDeCostos == null)
+            {
+                objCentroDeCostos = new CentroDeCostos();
+                objCentroDeCostos.codigo = "0";
+            }
+            ViewBag.codigo = new SelectList(_centroDeCostoRepo.getdescripcion(), "Codigo", "Descripcion", objCentroDeCostos.codigo);
+            return PartialView(objCentroDeCostos);
+        }
+
+        [HttpPost]
+
+        public ActionResult EditarCosto(CentroDeCostos model) {
+            var Salida = _centroDeCostoRepo.GrabarCentroDeCostos(model);
+            return RedirectToAction("costo", "home");
+        }
+
+        [HttpPost]
+
+        public ActionResult EditarModalC(int codigo)
+        {
+            var objCentroDeCostos = _centroDeCostoRepo.getCentroDeCostosById(codigo);
+            if(objCentroDeCostos == null)
+            {
+                objCentroDeCostos = new CentroDeCostos();
+                objCentroDeCostos.codigo = "0"; 
+            }
+            ViewBag.codigo = new SelectList(_centroDeCostoRepo.getdescripcion(), "descripcion", objCentroDeCostos.descripcion);
+            return PartialView(objCentroDeCostos);
+        }
+
+        [HttpPost]
+        public JsonResult EditarDesdeModalC (CentroDeCostos model)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(model.descripcion))
+                    throw new ArgumentException("Debes Ingresar Descripción");
+                var salida = _centroDeCostoRepo.GrabarCentroDeCostos(model);
+                if (salida == 1)
+                    return Json("OK");
+                else
+                    return Json("ERROR");
+            }
+            catch (Exception ex)
+            {
+                return Json("ERROR" + ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Eliminar(int codigo)
+        {
+            return Json(_centroDeCostoRepo.EliminarBycodigo(codigo));
+        }
+    } 
+}
---
+++ WebMVC_DevCpech/Repository/CentroDeCostoRepository.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebMVC_DevCpech.ContextDB;
+using WebMVC_DevCpech.Repository.IRepository;
+
+namespace WebMVC_DevCpech.Repository
+{
+    public class CentroDeCostoRepository
+    {
+        private static AlumnosEntities _db;
+        public CentroDeCostoRepository()
+        {
+            _db = new AlumnosEntities();
+        }
+      
+        /// Obtenemos Toda la imformacion de la tabla costos
+    
+        public List<CentroDeCostos> getCentroDeCostos()
+        {
+            return _db.CentroDeCostos.ToList();
+        }
+
+     
+        /// Obtenemos la descripción por el Codigo 
+   
+        internal CentroDeCostos getCentroDeCostosById(int codigo)
+        {
+            return _db.CentroDeCostos.Find(codigo);
+        }
+
+        /// Obtenemos todas las DESCRIPCIONES       
+        internal IEnumerable getdescripcion()
+        {
+            return _db.CentroDeCostos.ToList();
+        }
+
+        internal IEnumerable getCentroCostosAll()
+        {
+            return _db.CentroDeCostos.ToList();
+        }
+
+        internal int GrabarCentroDeCostos(CentroDeCostos model)
+        {
+            try
+            {
+                if (model.codigo == "0") 
+                    _db.CentroDeCostos.Add(model);
+                else
+                {
+                    // edicion
+                    var obj = _db.CentroDeCostos.Find(model.codigo);
+                    obj.descripcion = model.descripcion;
+                   
+                    _db.Entry(obj).State = EntityState.Modified;
+                }
+                _db.SaveChanges(); 
+                return 1;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+        }
+
+     
+        /// eliminar un registro
+        internal string EliminarBycodigo(int codigo)
+        {
+            try
+            {
+                var obj = _db.CentroDeCostos.Find(codigo);
+                _db.CentroDeCostos.Remove(obj);
+                _db.SaveChanges();
+                return "OK";
+            }
+            catch (Exception ex)
+            {
+                return "ERROR :" + ex.Message;
+            }
+
+        }
+    }
+}
